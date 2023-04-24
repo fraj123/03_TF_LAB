a9e860e0cd9ba70607b31e8698e962cfe3bb325c
Franz Mejia <mejiafranz@gmail.com>
2023-04-23 20:33:51 -0400

    fix: put right subnet

--- mv.tf
+++ mv.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "vm" {
   ami           = "ami-02396cdd13e9a1257"
-  subnet_id     = "subnet-03be59c9a3981f2e7"
+  subnet_id     = "subnet-011df66ce8d0700e9"
   instance_type = "t3.micro"
   tags = {
     Name = "my-first-tf-node"
