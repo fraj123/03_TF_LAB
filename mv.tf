22ed70409c4939c5c57bdda72e076749e615ca18
Franz Mejia <mejiafranz@gmail.com>
2023-04-23 20:32:24 -0400

    fix: put right ami for virginia region

--- mv.tf
+++ mv.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "vm" {
-  ami           = "ami-0578f2b35d0328762"
+  ami           = "ami-02396cdd13e9a1257"
   subnet_id     = "subnet-03be59c9a3981f2e7"
   instance_type = "t3.micro"
   tags = {
